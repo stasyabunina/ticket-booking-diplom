d5c68ab80a8e8fb9823404c8ed3973e0d84d1852
Anastasia <tryes9856@gmail.com>
2024-05-31 18:07:04 +0300

    add files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { HashRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './app/App.jsx';
